85e890fd345f100bdbc6bd14c5469f78d00e47d8
Sandip6509 <sandippatel3101@gmail.com>
2023-08-20 22:45:45 +0530

    custom react

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -1,10 +1,10 @@
 import Sam from "./sam"
 function App() {
-
+  const userName = "John"
   return (
     <>
       <Sam />
-      <h1>Hello Sandeep</h1>
+      <h1>Hello Sandeep {userName}</h1>
       <p>This is Sandeep Patel</p>
     </>
   )
--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!!!</h1>
+    </div>
+  );
+}
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target: '_blank'},
+    'Click me to visit google'
 )
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <App />  
+);

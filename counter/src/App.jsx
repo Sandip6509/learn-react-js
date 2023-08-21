1e3e68754b2ddf21da8c4849f44bae2f032ab8a4
Sandip6509 <sandippatel3101@gmail.com>
2023-08-21 21:58:43 +0530

    counter project complete

---
+++ counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(15)
+  const addValue = () => {
+    if(counter === 20){
+      return;
+    }
+    setCounter(counter + 1);
+  }
+  const removeValue = () => {
+    if(counter === 0){
+      return;
+    }
+    setCounter(counter - 1);
+  }
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <br/>
+      <button onClick={removeValue}>remove value</button>
+    </>
+  )
+}
+
+export default App

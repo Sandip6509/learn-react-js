91f3f15cbdbeb1d9f2d2784c8e022316ca4397f6
Sandip6509 <sandippatel3101@gmail.com>
2023-09-05 22:10:58 +0530

    password generator

---
+++ random-quote-generate/src/App.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [advice,setAdvice] = useState('')
+
+  useEffect(() => {
+    const url = "https://api.adviceslip.com/advice";
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setAdvice(json.slip.advice);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchData()
+  },[advice])
+
+  return (
+    <div className='app'>
+      <div className='card'>
+        <h1 className='heading'>{advice}</h1>
+        <button className='button'>
+          <span>GIVE ME ADVICE</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
